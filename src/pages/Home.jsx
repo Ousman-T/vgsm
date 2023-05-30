dcb8598b3e2e1c9ea15045ab29fb90d8e60f7386
Ousman Touray <otouray14@gmail.com>
2023-05-30 14:11:18 -0400

    testing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,24 @@
 
 function Home(){
     return(
-        <h1>Home Page</h1>
+        <div>
+            <h1>Welcome to VGSM</h1>
+
+            <h3>A simple site I designed in order to practice server handling</h3>
+
+        <div className="list">
+
+            <ul>I plan on Implementing:
+                <li>Logging In features</li>
+                <li>Posting methodology</li>
+                <li>Ability to interact with posts</li>
+                <li>Profiles for each user</li>
+                <li>Categories for posts</li>
+
+            </ul>
+        </div>
+        </div>
+
     )
 }
 
