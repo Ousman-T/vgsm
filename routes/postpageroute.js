ebec4b2439806f226f21fb7be6f5ad173b8b8b2e
Ousman Touray <otouray14@gmail.com>
2023-05-31 13:58:19 -0400

    setting boilerplate page for viewing posts in db/backend

--- routes/postpageroute.js
+++ routes/postpageroute.js
@@ -2,3 +2,10 @@
 
 const express = require('express');
 const router = express.Router();
+const postCtrl = require('../controllers/api/posting');
+
+// * POST Route
+router.post('/new', postCtrl.makePost);
+
+
+module.exports = router;
---
+++ src/pages/View.jsx
@@ -0,0 +1,10 @@
+
+
+function View(){
+    return(
+        <div>
+            <h1>View All Posts</h1>
+            <p>This page will be utilized to show all posts stored in backend/db</p>
+        </div>
+    )
+}
