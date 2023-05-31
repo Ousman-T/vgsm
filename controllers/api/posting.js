b8996ca3cb8ee00a62b53cb4b2944b60d45fea3e
Ousman Touray <otouray14@gmail.com>
2023-05-31 13:53:30 -0400

    wrote async function to make a post

--- controllers/api/posting.js
+++ controllers/api/posting.js
@@ -1,2 +1,19 @@
 // * Request handler logic
+const Create = require('../../models/create');
 
+async function makePost(req, res){
+    try{
+        console.log('controller functions, functioning');
+        const letPost = await Create.makePost(req.body);
+        console.log(letPost);
+        res.json(letPost);
+        
+    }catch(error){
+        console.log(error);
+        res.status(400).json(error)
+    };
+};
+
+module.exports = {
+    makePost
+}
