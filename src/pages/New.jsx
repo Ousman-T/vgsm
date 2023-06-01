90c2da2ba9e2a5fb7e37844aeaf1e492d6376ab6
Ousman Touray <otouray14@gmail.com>
2023-06-01 12:52:16 -0400

    working on form for req res and usestate

--- src/pages/New.jsx
+++ src/pages/New.jsx
@@ -1,14 +1,32 @@
 import { useState } from "react";
-
+import { createPost } from "../utilities/makePostApi";
 
 function New(){
-    const [newPost, setNewPost] = useState({})
-    return(
-        <div>
-            <h1>Create a post</h1>
-            <form action='/new' method='POST'>
-                Title:<input type="text" name="title"/>
+    const [newPost, setNewPost] = useState({
+        userName: "",
+        postTitle: "",
+        postBody: "",
+    });
 
+    const handleChange = (evt) => {
+        setNewPost({...newPost, [evt.target.userName]: evt.target.value, error: ""})
+    };
+
+    const handleSubmit = async (e) => {
+        console.log('Button clicked!');
+        e.preventDefault();
+    }
+    return(
+        <div className="newPost">
+            <h1>Create a post</h1>
+            <form action='/new' method="POST" onSubmit={handleSubmit}>
+                userName:<input type="text" name="userName" onChange={handleChange}/>
+                <br />
+                postTitle:<input type="text" name="postTitle" onChange={handleChange}/>
+                <br />
+                postBody:<input type="text" name="postBody" onChange={handleChange}/>
+                <br />
+                <button type="submit" onSubmit={handleSubmit}>Submit Info?</button>
             </form>
         </div>
 
